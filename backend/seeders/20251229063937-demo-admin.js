99a194e38edcca470112a08e5efe564764f9aaf9
salmaarayyan <salmaarayyan@gmail.com>
2026-01-04 15:39:40 +0700

    feat: add demo admin seeder for initial user setup with hashed password

---
+++ backend/seeders/20251229063937-demo-admin.js
@@ -0,0 +1,21 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const hashedPassword = await bcrypt.hash('admin123', 10);
+    
+    await queryInterface.bulkInsert('Users', [{
+      username: 'admin',
+      password: hashedPassword,
+      email: 'admin@studiokujogja.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
